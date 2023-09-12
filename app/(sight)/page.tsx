60e6d9593ef4709ae0ab3e69b22392d977685f07
Earl A. Villapaz <jasonburning16@gmail.com>
2023-09-12 18:54:02 +0800

    7: shadcn ui + packages

--- app/(sight)/page.tsx
+++ app/(sight)/page.tsx
@@ -1,7 +1,24 @@
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/InitialProfile";
 
 const SightPage = async () => {
     const profile = await initialProfile();
+
+    const server = await db.server.findFirst({
+        where: {
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    });
+
+    if (server) {
+        return redirect(`/servers/${server.id}`);
+    }
     
     return ( 
         <div>Create server: </div>
