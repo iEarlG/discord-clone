94d375922b98f960a3f8a594278a4f67cd18fa72
Earl A. Villapaz <jasonburning16@gmail.com>
2023-09-18 20:39:45 +0800

    10: Navigations p1

--- components/navigation/NavigationActions.tsx
+++ components/navigation/NavigationActions.tsx
@@ -2,12 +2,12 @@
 
 import { Plus } from "lucide-react";
 
-import { ActionTooltip } from "@/components/ActionToolTip";
+import { ActionTooltip } from "@/components/ActionTooltip";
 
 export const NavigationActions = () => {
     return (
         <div>
-            <ActionTooltip side="right" align="center" label="Add a server">
+            <ActionTooltip side="right" align="center" label="Add a Server">
                 <button className="flex items-center group">
                     <div className="flex items-center justify-center mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all 
                     overflow-hidden bg-background dark:bg-neutral-700 group-hover:bg-emerald-500">
