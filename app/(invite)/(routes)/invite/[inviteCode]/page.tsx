51060d89eb4b44a11699712459b6264a7bc4920f
Earl A. Villapaz <jasonburning16@gmail.com>
2023-09-22 20:34:57 +0800

    13: Server Create Invite & schema fixing

---
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -0,0 +1,66 @@
+
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { currentProfile } from "@/utils/currentProfile";
+import { db } from "@/lib/db";
+
+interface InviteCodePageProps {
+    params: {
+        inviteCode: string;
+    }
+};
+
+const InviteCodePage = async ({ params }: InviteCodePageProps) => {
+    const profile = await currentProfile();
+
+    if (!profile) {
+        return (
+            redirectToSignIn()
+        );
+    }
+
+    if (!params.inviteCode) {
+        return (
+            redirect("/")
+        );
+    }
+
+    const existingInviteServer = await db.server.findFirst({
+        where: {
+            inviteCode: params.inviteCode,
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }   
+        }
+    });
+
+    if (existingInviteServer) {
+        return (
+            redirect(`/servers/${existingInviteServer.id}`)
+        );
+    }
+
+    const server = await db.server.update({
+        where: {
+            inviteCode: params.inviteCode
+        },
+        data: {
+            members: {
+                create: [
+                    {
+                        profileId: profile.id   
+                    }
+                ]
+            }
+        }
+    });
+
+    return ( 
+        <div>InviteCodePage</div>
+    );
+}
+ 
+export default InviteCodePage;
