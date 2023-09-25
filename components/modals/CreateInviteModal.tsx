6c479995b29dabc8aec20c8884df1c9d62d8f82b
Earl A. Villapaz <jasonburning16@gmail.com>
2023-09-25 20:34:59 +0800

    13: Create Invite Modification

--- components/modals/CreateInviteModal.tsx
+++ components/modals/CreateInviteModal.tsx
@@ -51,7 +51,7 @@ export const CreateInviteModal = () => {
         <Dialog open={isModalOpen} onOpenChange={onClose}>  
             <DialogContent className="bg-white text-black p-0 overflow-hidden">
                 <DialogHeader className="pt-8 px-6">
-                    <DialogTitle className="text-center text-2xl font-bold">Invite Friends</DialogTitle>
+                    <DialogTitle className="text-center text-2xl font-bold">Invite Friends to <span className="text-cyan-500">{server?.name}</span></DialogTitle>
                 </DialogHeader>
                 <div className="p-6">
                     <Label className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
