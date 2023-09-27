e3adab73a6b8abe9886ef6a9fba8962598ace299
Earl A. Villapaz <jasonburning16@gmail.com>
2023-09-27 21:17:01 +0800

    18: Server Sidebar 2 (Search Control & KBD)

--- components/servers/ServerSearch.tsx
+++ components/servers/ServerSearch.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Search } from "lucide-react";
+
 interface ServerSearchProps {
     data: {
         label: string;
@@ -16,6 +18,18 @@ export const ServerSearch = ({
     data
 }: ServerSearchProps) => {
     return (
-        <div>ServerSearch</div>
+        <>
+            <button 
+                className="flex items-center w-full group px-2 py-2 rounded-md gap-x-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
+            >   
+            <Search className="w-4 h-4 text-zinc-50 dark:text-zinc-400" />
+                <p className="font-semibold text-xs text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition">
+                    Search
+                </p>
+                <kbd className="inline-flex items-center h-5 pointer-events-none select-none gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ml-auto">
+                    <span className="text-xs">Ctrl</span>K
+                </kbd>
+            </button>
+        </>
     )
 };
