c04f1c470dd90432cbec7f90a2a96bfc930d48c3
Earl A. Villapaz <jasonburning16@gmail.com>
2023-09-28 20:15:41 +0800

    18: Server Sidebar 2 (Search DONE)

--- components/servers/ServerSearch.tsx
+++ components/servers/ServerSearch.tsx
@@ -1,6 +1,10 @@
 "use client";
 
+import { useState } from "react";
+
 import { Search } from "lucide-react";
+
+import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
 
 interface ServerSearchProps {
     data: {
@@ -17,10 +21,13 @@ interface ServerSearchProps {
 export const ServerSearch = ({
     data
 }: ServerSearchProps) => {
+    const [isOpen, setIsOpen] = useState(false);
+    
     return (
         <>
             <button 
                 className="flex items-center w-full group px-2 py-2 rounded-md gap-x-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
+                onClick={() => setIsOpen(true)}
             >   
             <Search className="w-4 h-4 text-zinc-50 dark:text-zinc-400" />
                 <p className="font-semibold text-xs text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition">
@@ -30,6 +37,31 @@ export const ServerSearch = ({
                     <span className="text-xs">Ctrl</span>K
                 </kbd>
             </button>
+            <CommandDialog open={isOpen} onOpenChange={setIsOpen}>
+                <CommandInput 
+                    placeholder="Search for a channels or members"
+                />
+                <CommandList>
+                    <CommandEmpty>No results found</CommandEmpty>
+                    {data.map(({ label, type, data }) => {
+                        if (!data?.length) {
+                            return null
+                        };
+                        return (
+                            <CommandGroup key={label} heading={label}>
+                                {data?.map(({ id, icon, name }) => {
+                                    return (
+                                        <CommandItem key={id}>
+                                           {icon}
+                                           <span>{name}</span>
+                                        </CommandItem>
+                                    );
+                                })}
+                            </CommandGroup>
+                        );
+                    })}
+                </CommandList>
+            </CommandDialog>
         </>
     )
 };
